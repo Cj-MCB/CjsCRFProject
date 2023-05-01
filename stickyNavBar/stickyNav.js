88c2090b96f6e07cf00ca30d95a39c99365476fe
Cj-MCB <carsonjclegg@gmail.com>
2023-05-01 14:30:53 -0600

    Manual.html almost done

---
+++ stickyNavBar/stickyNav.js
@@ -0,0 +1,16 @@
+// Menu Bar Functions
+function homeButton() {
+    window.location.href="/home.html";
+}
+function galleryButton() {
+    window.location.href="/gallery.html";
+}
+function maintenanceButton() {
+    window.location.href="/maintenance.html";
+}
+function specsButton() {
+    window.location.href="/specs.html";
+}
+function manualButton () {
+    window.location.href="/manual.html";
+}
