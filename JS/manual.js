e672fd446357b234715dad687db3468b85c15a4d
Carson Clegg <26cclegg@go.dsdmail.net>
2023-05-02 12:49:17 -0600

    manualSizeAdj

--- JS/manual.js
+++ JS/manual.js
@@ -1,1 +1,13 @@
-// Make manual menu adjustable in size
+function fullSize() {
+    document.getElementById("manual").style.width = "1280px";
+    document.getElementById("manual").style.height = "720px";
+}
+function halfSIze() {
+    document.getElementById("manual").style.width = "640px";
+    document.getElementById("manual").style.height = "480px";
+}
+// Figure out how to get screen res then set
+function fitToScreen() {
+    document.getElementById("manual").style.width = "100%";
+    document.getElementById("manual").style.height = "100%";
+}
