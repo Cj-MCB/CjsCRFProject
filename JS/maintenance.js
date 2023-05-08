7f6e2b6183eac28bb1c3958c0a15b09dcff3feac
Carson Clegg <26cclegg@go.dsdmail.net>
2023-05-08 14:52:22 -0600

    somePDFtoPNG

--- JS/maintenance.js
+++ JS/maintenance.js
@@ -4,6 +4,7 @@ function openPage(value) {
     console.log("Page", pg, "on maintenance.html");
     const showpg = document.getElementById("showPage");
     showpg.style.visibility = "visible";
+    // Change to a png instead of pdf later
     showpg.innerHTML = '<object id="showpg" data="/media/manualRefrences/pdf/'+pg+'.pdf" type="application/pdf"></object>';
 }
 
