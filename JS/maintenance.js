7b5ad98f67a475fcae07d3b8fb754e43fd28357f
Carson Clegg <26cclegg@go.dsdmail.net>
2023-05-08 09:37:56 -0600

    showpgJSdone

--- JS/maintenance.js
+++ JS/maintenance.js
@@ -2,5 +2,12 @@ var pg=0;
 function openPage(value) {
     pg = value;
     console.log("Page", pg, "on maintenance.html");
-    document.getElementById("showPage").style.visibility = "visible";
+    const showpg = document.getElementById("showPage");
+    showpg.style.visibility = "visible";
+    showpg.innerHTML = '<object id="showpg" data="/media/manualRefrences/pdf/'+pg+'.pdf" type="application/pdf"></object>';
+}
+
+function closePage() {
+    const showpg = document.getElementById("showPage");
+    showpg.style.visibility = "hidden";
 }
