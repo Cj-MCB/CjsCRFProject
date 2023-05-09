f59a092e80512b9b5a45dd8c4424c0a50e98005a
Carson Clegg <carsonjclegg@gmail.com>
2023-05-09 14:51:59 -0600

    fixedMaintPageSize

--- JS/maintenance.js
+++ JS/maintenance.js
@@ -1,11 +1,11 @@
 var pg=0;
+
 function openPage(value) {
     pg = value;
     console.log("Page", pg, "on maintenance.html");
     const showpg = document.getElementById("showPage");
+    showpg.innerHTML = '<object id="showpg" data="/media/manualRefrences/png/'+pg+'.png" type="application/pdf"></object>';
     showpg.style.visibility = "visible";
-    // Change to a png instead of pdf later
-    showpg.innerHTML = '<object id="showpg" data="/media/manualRefrences/pdf/'+pg+'.pdf" type="application/pdf"></object>';
 }
 
 function closePage() {
